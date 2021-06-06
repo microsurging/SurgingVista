2d33c0767c06de452990ac36c3c6d457d45907bf
fanliang11 <137629448@qq.com>
2021-06-07 00:03:11 +0800

    fix memory leak problem based on routepath call

--- src/Surging.Core/Surging.Core.ProxyGenerator/Implementation/ServiceProxyProvider.cs
+++ src/Surging.Core/Surging.Core.ProxyGenerator/Implementation/ServiceProxyProvider.cs
@@ -52,22 +52,24 @@ namespace Surging.Core.ProxyGenerator.Implementation
             T result = default(T);
             if (!string.IsNullOrEmpty(serviceKey))
             {
-                var proxy = ServiceResolver.Current.GetService<RemoteServiceProxy>(serviceKey);
-                if (proxy == null)
-                {
-                    proxy = new RemoteServiceProxy(serviceKey, _serviceProvider);
-                    ServiceResolver.Current.Register(serviceKey, proxy);
-                }
+               // var proxy = ServiceResolver.Current.GetService<RemoteServiceProxy>(serviceKey);
+               var proxy = new RemoteServiceProxy(serviceKey, _serviceProvider);
+                //if (proxy == null)
+                //{
+                //    proxy = new RemoteServiceProxy(serviceKey, _serviceProvider);
+                //    ServiceResolver.Current.Register(serviceKey, proxy);
+                //}
                 result = await proxy.Invoke<T>(parameters, serviceRoute.ServiceDescriptor.Id);
             }
             else
             {
-                var proxy = ServiceResolver.Current.GetService<RemoteServiceProxy>();
-                if (proxy == null)
-                {
-                    proxy = new RemoteServiceProxy(null, _serviceProvider);
-                    ServiceResolver.Current.Register(null, proxy);
-                }
+                //var proxy = ServiceResolver.Current.GetService<RemoteServiceProxy>();
+                var proxy = new RemoteServiceProxy(serviceKey, _serviceProvider);
+                //if (proxy == null)
+                //{
+                //    proxy = new RemoteServiceProxy(null, _serviceProvider);
+                //    ServiceResolver.Current.Register(null, proxy);
+                //}
                 result = await proxy.Invoke<T>(parameters, serviceRoute.ServiceDescriptor.Id);
             }
             return result;
